0e184ea4396cfe3fea85c979f8950272028b4073
cph1014 <79871083+cph1014@users.noreply.github.com>
2021-06-24 11:03:46 +0100

    drum v2

---
+++ week_4/Day 18/warmup.js
@@ -0,0 +1,11 @@
+let a= ["pear, waterlemon"];
+let b= 2;
+let c= true;
+
+let arr=["apple","orange","pipeapple","lemon"];
+arr.push("papaya");
+console.log(arr);
+
+for (let d=0; d<arr.length; d++){
+    console.log(arr[d]);
+    }
